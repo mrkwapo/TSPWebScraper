be07cb99a98f6441aba3a70dd356eb0814443468
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-04-07 09:49:36 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 package com.Dracodess;
 
-import java.text.ParseException;
-
 public class Main {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         Report.getSharePricesByDateRange();
     }
 }
